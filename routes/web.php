af8c5c84f54279c14d64253254294f681e79e3e0
cemonk <dnadzaky@gmail.com>
2025-12-01 10:08:01 +0700

    dzaky nabil
    controller user, add role usser, route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::prefix('api')->group(function () {
+    Route::post('/register/customer', [AuthController::class, 'registerCustomer']);
+    Route::post('/register/admin', [AuthController::class, 'registerAdmin']);  // optional
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::get('/', function () {
     return view('welcome');
